bb61c86359bf3cfa716ea11e5f20bb9be97d6426
andrewk7 <andrewk717@gmail.com>
2018-03-11 22:45:29 -0700

    Preliminary working with bench press data

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,17 +1,59 @@
 source("../firstmodel.R")
+library(plotly)
+
 
 server <- function(input, output) {
-
- #  output$getUpdatedQH3 <- reactive({
-#    total <- (housingVariables(input$QH1) + housingVariables(input$QH2) + 6 + housingVariables(input$QH4) + housingVariables(input$QH5) )
-#    predictedHappiness(total / 5, input$Age, input$Gender, input$School)
-#  })
   
-  output$asdf <- reactive({
-    predict(model1,  data.frame(Initial.Training.Status..years. = input$selectYearsTrained, With.or.Without ="With"))
+
+  output$bench_plot<- renderPlotly({plot_ly(, x=c("With creatine", "Without Creatine"), 
+                                     y=c( predict(model1,  data.frame(Initial.Training.Status..years. = input$selectYearsTrained,
+                                                                      With.or.Without ="With")), 
+                                          predict(model1,  data.frame(Initial.Training.Status..years. = input$selectYearsTrained,
+                                                                      With.or.Without ="Without"))), type="bar", 
+                                     marker = list(color = c('purple', 'gold'))) %>%  
+      layout(title = "Bench Press Improvement", xaxis = list(title = "Creatine Usage"), yaxis = list(title = "Percentage of Increase"))
     })
-
   
+   
+
+
+  output$yes_creatine_response <- reactive({
+    years_response <- predict(model1,  data.frame(Initial.Training.Status..years. = input$selectYearsTrained,
+                                                  With.or.Without ="With"))
+    years_response <- as.numeric(years_response)
+    round(years_response, digits = 2)
+    new_bench <- (1 + years_response/100) * as.numeric(input$inputBenchWeight)
+    
+    text <- paste("If you have worked out for ", input$selectYearsTrained,
+                  " years you would increase your Bench 1RM by ", round(years_response, digits = 2), 
+                  "% with creatine. You would go from having a ", input$inputBenchWeight, " to having
+                  a ",  round(new_bench, digits = 2), " 1RM.", sep = "")
+  })
+  
+  
+  output$no_creatine_response <- reactive({
+    years_response <- predict(model1,  data.frame(Initial.Training.Status..years. = input$selectYearsTrained,
+                                                  With.or.Without ="Without"))
+    years_response <- as.numeric(years_response)
+    new_bench <- (1 + years_response/100) * as.numeric(input$inputBenchWeight)
+    text <- paste("If you have worked out for ", input$selectYearsTrained,
+                  " years you would increase your Bench 1RM by ", round(years_response, digits = 2), 
+                  "% without creatine. You would go from having a ", input$inputBenchWeight, " to having
+                  a ",  round(new_bench, digits = 2), " 1RM.", sep = "")
+  })
+
+  asdf <- reactive({
+    years_response <- predict(model1,  data.frame(Initial.Training.Status..years. = input$selectYearsTrained,
+                                                  With.or.Without ="Without"))
+    years_response <- as.numeric(years_response)
+    new_bench <- (1 + years_response/100) * as.numeric(input$inputBenchWeight)
+    text <- paste("If you have worked out for ", input$selectYearsTrained,
+                  " years you would increase your Bench 1RM by ", round(years_response, digits = 2), 
+                  "% without creatine. You would go from having a ", input$inputBenchWeight, " to having
+                  a ",  round(new_bench, digits = 2), " 1RM.", sep = "")
+  })
+
+
   output$inputBodyInfo <- renderPrint({input$inputBodyWeight})
   output$inputGenderInfo <- renderPrint({input$selectGender})
 
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,5 +1,9 @@
+
+library(shinythemes)
+
 # Define UI for app that predicts how much strength increase with and without creatine ----
-ui <- fluidPage(
+
+ui <- fluidPage( theme = shinytheme("cerulean"),
   
   # App title ----
   titlePanel("Team Awesome Possum"),
@@ -15,7 +19,7 @@ ui <- fluidPage(
              ),
              tabPanel("Unlock your Potential",
                       sidebarLayout(
-                        
+
                         # Sidebar panel for inputs ----
                         sidebarPanel(
                           
@@ -28,32 +32,38 @@ ui <- fluidPage(
                                       selected = 'm'),
                           
                           #Years Trained Input
-                          textInput("selectYearsTrained", label = h3("Years Trained"), value =1),
+                          numericInput("selectYearsTrained", label = h3("Years Trained"), value = 1),
                           
                           #Bench Output
-                          textInput("inputBenchWeight", label = h3("Bench weight input"), placeholder = "Enter Bench Weight..."),
+                          textInput("inputBenchWeight", label = h3("Bench weight input"), value = 135),
                           
                           #Squat Input
-                          textInput("inputSquatWeight", label = h3("Squat weight input"), placeholder = "Enter Squat Weight...")
+                          textInput("inputSquatWeight", label = h3("Squat weight input"), value = 225)
                         ),
                         
                         # Main panel for displaying outputs ----
                         mainPanel(
-                          #Testing to see if inputs are going in
-                          textOutput("inputBodyInfo"),
-                          textOutput("inputGenderInfo"),
-                          textOutput("inputYearsInfo"),
-                          textOutput("inputBenchInfo"),
-                          textOutput("inputSquatInfo"),
+                        
+                          tags$strong("About the calculator:"),
+                          p("When committing to a fitness journey, people often debate
+                            whether taking supplements would be appropriate. The effect of supplements,
+                            particularly creatine, will vary between people, and depends on factors such 
+                            as body weight, current strength, etc. To guide your decision and estimate 
+                            your potential growth, our calculator takes the most important factors and reports
+                            your projected fitness growth, with and without creatine use."),
+                          br(),
                           
+                          tags$strong("Here are your possible lifts without creatine:"),
+                          textOutput("no_creatine_response"),
+                          br(),
+                          tags$strong("Here are your possible lifts with creatine:"),
+                          textOutput("yes_creatine_response"),
+
                           
-                          p("Here is your potential without creatine:"),
-                          #plotOutput(outputId = "noCreatineModelPlot)
-                          textOutput("asdf"),
-                          p("Here is FULL POTENTIAL:")
-                          #plotOutput(outputId = "noCreatineModelPlot)
-                          #Output: ModelPlot 
-                          # plotOutput(outputId = "modelPlot")
+                          br(),
+                          tags$strong("Here is a graphical comparison"),
+                          plotlyOutput("bench_plot")
+                 
                         )
              )
     )
