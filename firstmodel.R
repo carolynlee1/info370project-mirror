62ed5d40d6879092fe3eef64a87d263e7adf0504
Carolyn Lee <carolynl@uw.edu>
2018-03-11 23:44:04 -0700

    added new data and updated model

--- firstmodel.R
+++ firstmodel.R
@@ -3,8 +3,22 @@ library(ggplot2)
 library(rpart)
 library(statisticalModeling)
 View(d3)
-model1 <- rpart(BP.1RM ~ Initial.Training.Status..years. + With.or.Without, data = d3)
-model1
+model1 <- rpart(benchpress ~ Initial.Training + Creatine..y.n. + Sex, data = d3, cp=0.001)
+
 fmodel(model1)
+ggplot(data = d3, mapping=aes(x=benchpress, y=Sex, color=Creatine..y.n.)) + geom_point()
 
 evaluate_model(model1)
+library(plotrix)
+num <- jiggle(19, range=c(40,50))
+df <- data.frame(num)
+
+GetJiggle <- function(old.df, n, min, max) {
+  list <- jiggle(n, range=c(min,max))
+  View(list)
+  list <- data.frame(unname(list))
+  
+  master.df <- rbind(old.df, list)
+  return(master.df)
+}
+
