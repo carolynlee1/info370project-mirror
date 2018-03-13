28cca5000f429ab1772c2a706a4db34e2b2d5be7
Carolyn Lee <carolynl@uw.edu>
2018-03-12 18:50:41 -0700

    fix data again

--- firstmodel.R
+++ firstmodel.R
@@ -1,28 +1,19 @@
 
-d3 <- read.csv("../initial training and sex data.csv")
+d3 <- read.csv("~/370/group-formation-for-projects-team-awesome-possum/condensed.csv")
 library(ggplot2)
 library(rpart)
 library(statisticalModeling)
+library(dplyr)
 View(d3)
 model1 <- rpart(benchpress ~ Initial.Training + Creatine, data = d3, cp=0.001)
 fmodel(model1)
 
+new.d3 <- d3 %>% 
+  filter(Sex == "Male")
+
 ggplot(data = d3, mapping=aes(x=benchpress, y=Initial.Training, color=Creatine)) + geom_point()
 
 evaluate_model(model1)
-#
-#library(plotrix)
-#num <- jiggle(19, range=c(40,50))
-#df <- data.frame(num)
-
-#GetJiggle <- function(old.df, n, min, max) {
-# list <- jiggle(n, range=c(min,max))
-# View(list)
-# list <- data.frame(unname(list))
-  
-# master.df <- rbind(old.df, list)
-# return(master.df)
-#}
 
 test <- evaluate_model(model1)
 
