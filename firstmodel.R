83d9751222b790cf737bb95cf5cb925319b983a2
Carolyn Lee <carolynl@uw.edu>
2018-03-12 23:07:03 -0700

    edited model names and add comments

--- firstmodel.R
+++ firstmodel.R
@@ -1,15 +1,17 @@
 
-d3 <- read.csv("~/370/group-formation-for-projects-team-awesome-possum/initial training and sex data.csv")
+d3 <- read.csv("../initial training and sex data.csv")
 library(ggplot2)
 library(rpart)
 library(statisticalModeling)
-library(dplyr)
 View(d3)
-
-bp.model <- rpart(benchpress ~ Initial.Training + Creatine + Sex, data = d3, cp=0.001)
-squat.model <- rpart(Squat ~ Initial.Training + Creatine + Sex, data = d3, cp=0.001)
-fmodel(bp.model)
-fmodel(squat.model)
+#benchpress model
+model1 <- rpart(benchpress ~ Initial.Training + Creatine + Sex, data = d3, cp=0.001)
+#squat model
+model2 <- rpart(Squat ~ Initial.Training + Creatine + Sex, data = d3, cp=0.001)
+#visualizing models
+fmodel(model1)
+fmodel(model2)
+#check model plot
 ggplot(data = d3, mapping=aes(x=benchpress, y=Initial.Training, color=Creatine)) + geom_point()
 
 evaluate_model(model1)
