1e91fa3975bef217f9424eef1a376d8d3013420a
Cherie Ruan <cherie_ruan@hotmail.com>
2018-03-12 18:15:32 -0700

    Fix bodyweight model

--- bodyweight-model.R
+++ bodyweight-model.R
@@ -7,3 +7,7 @@ d3 <- read.csv("../bodyweight-data.csv")
 bodyMassModel <- lm(Diff ~ Age + Creatine, data = d3)
 d3
 fmodel(bodyMassModel)
+
+new <- data.frame(Age = c(20), Creatine = c(1)) # input go here
+predict(bodyMassModel, new)
+
